9add0c88d08e54c8dc01e538d208ae2da11dd90d
Charlton Smith <CharltonSmith@outlook.com>
2018-04-29 02:53:07 -0400

    changed skills

--- src/components/skills/Skills.vue
+++ src/components/skills/Skills.vue
@@ -1,27 +1,27 @@
 <template>
     <div class="hello">
-        <p>{{ name }}</p>
-        <div class="holder">
-            <form @submit.prevent="addSkill">
-                <input type="text" placeholder="Enter a skill you have..." v-model="skill" v-validate="'min:5'"
-                       name="skill">
-                <transition name="alert-in" enter-active-class="animated flipInX"
-                            leave-active-class="animated flipOutX">
-                    <p class="alert" v-if="errors.has('skill')">{{errors.first('skill')}}</p>
-                </transition>
-            </form>
-            <ul>
-                <transition-group name="list" enter-active-class="animated bounceInUp"
-                                  leave-active-class="animated bounceOutDown">
-                    <li v-for="(data, index) in skills" :key="index">
-                        {{data.skill}}
-                        <i class="fa fa-minus-circle" v-on:click="remove(index)"></i>
-                    </li>
-                </transition-group>
-            </ul>
+        <div class="container">
 
-            <p v-if="skills.length >= 1">You have more than 1 skill</p>
-            <p v-else>You have less than or equal to 1 skills</p>
+            <h2 class="header">{{ name }}</h2>
+            <div class="holder">
+                <form @submit.prevent="addSkill">
+                    <input type="text" placeholder="Enter a skill you have..." v-model="skill" v-validate="'min:5'"
+                           name="skill">
+                    <transition name="alert-in" enter-active-class="animated flipInX"
+                                leave-active-class="animated flipOutX">
+                        <p class="alert" v-if="errors.has('skill')">{{errors.first('skill')}}</p>
+                    </transition>
+                </form>
+                <ul>
+                    <transition-group name="list" enter-active-class="animated bounceInUp"
+                                      leave-active-class="animated bounceOutDown">
+                        <li v-for="(data, index) in skills" :key="index">
+                            {{data.skill}}
+                            <i class="fa fa-minus-circle" v-on:click="remove(index)"></i>
+                        </li>
+                    </transition-group>
+                </ul>
+            </div>
 
         </div>
     </div>
@@ -32,7 +32,7 @@
         name: 'Skills',
         data() {
             return {
-                name: 'My Programming Skills',
+                name: 'Add 5 Skills',
                 btnState: true,
                 skills: [
                     {"skill": "Vue.js"},
@@ -47,7 +47,7 @@
         methods: {
             addSkill() {
                 this.$validator.validateAll().then((result) => {
-                    if (result) {
+                    if (result && this.skills.length < 5) {
 
                         this.skills.push({skill: this.skill})
                         this.skill = '';
